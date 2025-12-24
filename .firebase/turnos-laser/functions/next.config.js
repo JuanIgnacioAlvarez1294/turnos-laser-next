75b709ab9a745017323462608dba2b9481097e8e
Juan Ignacio Alvarez <nacho1294.ji@gmail.com>
2025-12-24 11:37:45 -0300

    chore: remove Firebase Functions and legacy webhooks

---
+++ .firebase/turnos-laser/functions/next.config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// next.config.js
+var nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  images: {
+    unoptimized: true
+  }
+};
+module.exports = nextConfig;
