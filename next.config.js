8fcbabab57a9cac7de4311452b4a21e6fcf644f4
Juan Ignacio Alvarez <nacho1294.ji@gmail.com>
2025-12-27 23:51:50 -0300

    perf(next): improve image optimization configuration

--- next.config.js
+++ next.config.js
@@ -12,7 +12,16 @@ const nextConfig = {
   },
 
   images: {
-    unoptimized: true,
+
+    unoptimized: false,
+    
+    formats: ['image/avif', 'image/webp'],
+
+    deviceSizes: [320, 375, 420, 640, 768, 1024, 1280],
+    imageSizes: [200, 300, 400, 520, 640, 800],
+
+    // 🧠 Cache largo (Firebase lo aprovecha)
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 días
   },
 };
 
